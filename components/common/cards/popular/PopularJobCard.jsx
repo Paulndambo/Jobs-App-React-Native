df17e151f1d03923f1f055c9d29a221dc1458eca
Paul Ndambo <paulkadabo@gmail.com>
2023-04-29 09:06:26 +0300

    first commit

---
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { View, Text, TouchableOpacity, Image } from 'react-native';
+
+import { checkImageURL } from '../../../../utils';
+
+import styles from './popularjobcard.style'
+
+const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
+  return (
+    <TouchableOpacity 
+      style={styles.container(selectedJob, item)}
+      onPress={() => handleCardPress(item)}
+    >
+      <TouchableOpacity
+        style={styles.logoContainer(selectedJob, item)}
+      >
+        <Image 
+          source={{uri: checkImageURL(item.employer_logo) ? item.employer_logo : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKGQB_hmUVkx0HNjVmAKctp_E1gET48ocWSiKm&s=0" }} 
+          resizeMode="contain" 
+          style={styles.logoImage} 
+        />
+      </TouchableOpacity>
+
+      <Text style={styles.companyName} numberOfLines={1}>{item.employer_name}</Text>
+      <View style={styles.infoContainer}>
+      <Text style={styles.jobName(selectedJob, item)} numberOfLines={1}>{item.job_title}</Text>
+      <Text style={styles.location}>{item.job_country}</Text>
+      </View>
+      
+    </TouchableOpacity>
+  )
+}
+
+export default PopularJobCard
